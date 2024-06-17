25983824d59aa29391bfb981df4d4357411fee80
Yash220402 <yash220402@gmail.com>
2024-06-17 11:55:25 +0530

    first commit

---
+++ donut.py
@@ -0,0 +1,41 @@
+# DONUT Document Parsing
+import re
+from transformers import DonutProcessor, VisionEncoderDecoderModel
+from datasets import load_dataset
+import torch
+from PIL import Image, ImageShow
+from pprint import pprint
+
+processor = DonutProcessor.from_pretrained("naver-clova-ix/donut-base-finetuned-cord-v2")
+model = VisionEncoderDecoderModel.from_pretrained("naver-clova-ix/donut-base-finetuned-cord-v2")
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model.to(device)
+# load document image
+# dataset = load_dataset("hf-internal-testing/example-documents", split="test")
+# image = dataset[2]["image"]
+
+image_path = "invoice3.jpg"
+image = Image.open(image_path)
+
+# prepare decoder inputs
+task_prompt = "<s_cord-v2>"
+decoder_input_ids = processor.tokenizer(task_prompt, add_special_tokens=False, return_tensors="pt").input_ids
+
+pixel_values = processor(image, return_tensors="pt").pixel_values
+
+outputs = model.generate(
+    pixel_values.to(device),
+    decoder_input_ids=decoder_input_ids.to(device),
+    max_length=model.decoder.config.max_position_embeddings,
+    pad_token_id=processor.tokenizer.pad_token_id,
+    eos_token_id=processor.tokenizer.eos_token_id,
+    use_cache=True,
+    bad_words_ids=[[processor.tokenizer.unk_token_id]],
+    return_dict_in_generate=True,
+)
+
+sequence = processor.batch_decode(outputs.sequences)[0]
+sequence = sequence.replace(processor.tokenizer.eos_token, "").replace(processor.tokenizer.pad_token, "")
+sequence = re.sub(r"<.*?>", "", sequence, count=1).strip()  # remove first task start token
+pprint(processor.token2json(sequence))
---
+++ ocr.py
@@ -0,0 +1,70 @@
+from PIL import Image, ImageDraw
+import cv2
+from pathlib import Path
+import easyocr
+import matplotlib.pyplot as plt
+from pprint import pprint as pp
+from tqdm import tqdm
+import json
+
+
+def create_bounding_box(bbox_data):
+    xs = []
+    ys = []
+    for x, y in bbox_data:
+        xs.append(x)
+        ys.append(y)
+    
+    left = int(min(xs))
+    top = int(min(ys))
+    right = int(max(xs))
+    bottom = int(max(ys))\
+    
+    return [left, top, right, bottom]
+
+# Open image
+image_path = "invoice4.jpg"
+image = Image.open(image_path)
+width, height = image.size
+scale = 1
+image = image.resize((int(width*scale), int(height*scale)))
+image.save(str(image_path))
+
+reader = easyocr.Reader(['en'])
+ocr_result = reader.readtext(str(image_path))
+
+# Create rectanlges
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(28, 28))
+
+left_image = Image.open(image_path).convert("RGB")
+right_image = Image.new("RGB", left_image.size, (255, 255, 255))
+
+left_draw = ImageDraw.Draw(left_image)
+right_draw = ImageDraw.Draw(right_image)
+
+for i, (bbox, word, confidence) in enumerate(ocr_result):
+    box = create_bounding_box(bbox)
+
+    left_draw.rectangle(box, outline="blue", width=2)
+    left, top, right, bottom = box
+
+    left_draw.text((right + 5, top), text=str(i + 1), fill="red") 
+    right_draw.text((left, top), text=word, fill="black")
+    
+ax1.imshow(left_image)
+ax2.imshow(right_image)
+ax1.axis("off");
+ax2.axis("off");
+
+
+ocr_result = reader.readtext(str(image_path), batch_size=16)
+ocr_page = []
+for bbox, word, confidence in ocr_result:
+    ocr_page.append({
+        "word": word, "bounding_box": create_bounding_box(bbox)
+    })
+
+for i in range(len(ocr_page)):
+    print(ocr_page[i])
+
+plt.show()
